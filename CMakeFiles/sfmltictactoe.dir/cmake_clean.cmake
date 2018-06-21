c73468adf168611917263a5eea2ab3663b45fab8
demi <meintomatenfrosch@gmail.com>
2018-06-20 23:12:49 -0400

    initial commit

---
+++ CMakeFiles/sfmltictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfmltictactoe.dir/main.cpp.o"
+  "sfmltictactoe.pdb"
+  "sfmltictactoe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfmltictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(800, 800), "Tricked Out TicTacToe in SFML", Style::Close | Style::Titlebar | Style::Resize);
+    
+    
+}
